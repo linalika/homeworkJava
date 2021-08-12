b21416455a0eda6ddd37054045089b34f4ed2a14
alina stepanova <gklina9@yandex.ru>
2021-08-12 20:15:24 +0300

    исправления

--- src/main/java/homework5/Staff.java
+++ src/main/java/homework5/Staff.java
@@ -1,8 +1,5 @@
 package homework5;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Staff {
     public static void main(String[] args) {
         String[] fullName = {"VМаксим", "Елена", "Аркадий","Айндрей", "Мария"};
@@ -11,7 +8,8 @@ public class Staff {
         String [] phoneNumber = {"79898989","79498989","79867989","78983456723","79898989"};
         int [] salary = {59000,89234,120897,78948,80900};
         int [] age = {34,45,38,44,38};
-        Employee[] staff = new Employee[5];
+        //как можно реализовать это через map? пыталась создать словарь с вложенными словарями, и получать по ключу
+       Employee[] staff = new Employee[5];
         for (int i=0 ; i< staff.length; i++) {
             staff[i] = new Employee(fullName[i],position[i],email[i],phoneNumber[i],salary[i],age[i]);
         }
@@ -19,7 +17,5 @@ public class Staff {
             staff[i].info();
         }
     }
-
-
 
 }
