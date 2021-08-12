e05fc974259b2c631df2b5da564c83c237148db6
alina stepanova <gklina9@yandex.ru>
2021-08-12 20:56:15 +0300

    исправления

--- src/main/java/homework5/Staff.java
+++ src/main/java/homework5/Staff.java
@@ -8,8 +8,6 @@ public class Staff {
         String [] phoneNumber = {"79898989","79498989","79867989","78983456723","79898989"};
         int [] salary = {59000,89234,120897,78948,80900};
         int [] age = {34,45,38,44,38};
-        //как можно реализовать это через map? пыталась создать словарь с вложенными словарями, и получать по ключу
-        //хочу создать что то подобное {'key' : {'key':value, 'key':value}, key' : {'key':value, 'key':value,}}
        Employee[] staff = new Employee[5];
         for (int i=0 ; i< staff.length; i++) {
             staff[i] = new Employee(fullName[i],position[i],email[i],phoneNumber[i],salary[i],age[i]);
